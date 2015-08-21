114a3d19f833c1ee692617ea3245ce50419f717a
papadalin <519121574@qq.com>
2015-08-22 00:15:23 +0800

    big

--- 7-15-table-bianping.r
+++ 7-15-table-bianping.r
@@ -6,8 +6,15 @@ table2flat <- function(mytable) {
         for (i in 1:rows){
                 for (j in 1:df$Freq[i]){
                         row <- df[i,c(1:(cols-1))]
-                        x <- rbind(x.row)
+                        x <- rbind(x,row)
                 }
         }
         row.names(x) <- c(1:dim(x)[1])
+        return(x)
 }
+treatment <- rep(c('Placebo', "Treated"), times = 3)
+improved <- rep(c("None", "Some", "Marked"), each = 2)
+Freq <- c(29,13,7,17,7,21)
+mytable <- as.data.frame(cbind(treatment, improved, Freq))
+mydata <- table2flat(mytable)
+
